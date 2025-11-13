1eb98e15236c111b5d6fc536faf3b6b40b82aea6
kanghyunseong <gustjd2754@daum.net>
2025-11-13 16:32:17 +0900

    123456

--- front/src/component/Common/Nav/Nav.jsx
+++ front/src/component/Common/Nav/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
   return (
     <NavContainer>
       <NavTitle>서비스 목록</NavTitle>
-      <NavLink href="/reservation">차량 찾기</NavLink>
+      <NavLink href="/cars/searchList">차량 찾기</NavLink>
       <NavLink href="/support">충전소</NavLink>
       <NavLink href="/community">커뮤니티</NavLink>
     </NavContainer>
