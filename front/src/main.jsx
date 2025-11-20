519614fd57b7812a0590653cebc4263ad0d10f65
kanghyunseong <gustjd2754@daum.net>
2025-11-20 10:27:36 +0900

    123:

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { AuthContext } from "./context/AuthContext.jsx";
+import { AuthContext, AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <AuthContext>
+  <AuthProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </AuthContext>
+  </AuthProvider>
 );
